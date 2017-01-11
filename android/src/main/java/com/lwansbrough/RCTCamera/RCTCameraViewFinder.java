d2f4c7592b504907b0f3e3b58af99cde0f3635b3
Rodrigo Crespo <rcrespocepeda@gmail.com>
2017-01-11 23:07:39 +0100

    try to emit array

--- android/src/main/java/com/lwansbrough/RCTCamera/RCTCameraViewFinder.java
+++ android/src/main/java/com/lwansbrough/RCTCamera/RCTCameraViewFinder.java
@@ -312,7 +312,7 @@ class RCTCameraViewFinder extends TextureView implements TextureView.SurfaceText
                 WritableMap event = Arguments.createMap();
                 // String encoded = Base64.encodeToString(imageData, Base64.DEFAULT);
                 // event.putString("data", encoded);
-                event.putArray("data", Arguments.fromArray(imageData))
+                event.putArray("data", Arguments.fromArray(imageData));
 
                 // event.putString("type", result.getBarcodeFormat().toString());
                 reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit("PreviewFrameReadAndroid", event);
